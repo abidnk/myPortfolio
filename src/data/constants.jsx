6857679df2f099d2053e7bbc031e9b08993e59c9
abidnk <abidnk34@gmail.com>
2024-01-23 14:44:33 +0530

    first commit

---
+++ src/data/constants.jsx
@@ -0,0 +1,283 @@
+export const Bio = {
+    name: "Abid NK",
+    roles: [
+      "Front-End Developer",
+      "React Developer",
+      "UI/UX Designer",
+      "Programmer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/abidnk",
+    resume:
+      "https://drive.google.com/file/d/1ODtKNnP5jSlY1sV2PnmTm8LAM1cxXG0z/view?usp=sharing",
+    linkedin: "https://linkedin.com/in/abid-nk-545b42247",
+    twitter: "https://twitter.com/abidnk2002",
+    insta: "https://instagram.com/abid.n.k",
+    facebook: "https://www.facebook.com/",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "Redux",
+          image:
+            "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+        },
+        {
+          name: "Next Js",
+          image:
+            "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        {
+          name: "Material UI",
+          image:
+            "https://mui.com/static/logo.png",
+        },
+        {
+          name: "Flutter",
+          image:
+            "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+        },
+        {
+          name: "TypeScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Typescript.svg/2048px-Typescript.svg.png",
+        },
+      ],
+    },
+    {
+      title: "Android",
+      skills: [
+        {
+          name: "Dart",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/c/c6/Dart_logo.png",
+        },
+        {
+          name: "React Native",
+          image:
+            "https://toppng.com/uploads/preview/react-native-svg-transformer-allows-you-import-svg-aperture-science-innovators-logo-11562851994zqcpwozsvy.png",
+        },
+        {
+          name: "Expo Go",
+          image:
+            "https://cdn.icon-icons.com/icons2/2389/PNG/512/expo_logo_icon_145293.png",
+        },
+        {
+          name: "Android Studio",
+          image:
+            "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "Vercel",
+          image:
+            "https://w7.pngwing.com/pngs/436/888/png-transparent-vercel-hd-logo-thumbnail.png",
+        },
+        {
+          name: "Netlify",
+          image:
+            "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://vectorseek.com/wp-content/uploads/2023/05/Postman-Logo-Vector.jpg",
+        },
+        {
+          name: "Wordpress",
+          image:
+            "https://w7.pngwing.com/pngs/510/329/png-transparent-wordpress-com-blog-computer-icons-wordpress-emblem-trademark-logo.png",
+        },
+        {
+          name: "Figma",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/a/ad/Figma-1-logo.png",
+        },
+        {
+          name: "Linux",
+          image:
+            "https://pngimg.com/d/linux_PNG1.png",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+  
+    {
+      id: 0,
+      img: "https://media.licdn.com/dms/image/D560BAQEvhp-UN48JIw/company-logo_200_200/0/1695803239077?e=2147483647&v=beta&t=9om6Zf94HuN_g_Zd65GzLs5WubSMRKNQ1FYrRLv-xPg",
+      role: "React developer ",
+      company: "Bridgeon Solutions",
+      date: "May 2023 - Present",
+      desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "Next.Js",
+        "Material UI",
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "TypeScript",
+        "Dart",
+        "Flutter",
+        "Bootstrap"
+      ],
+      
+    },
+   
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://www.static-contents.youth4work.com/university/Documents/Colleges/collegeLogo/1675427.png?v=20190301143032",
+      school: "Government College Mokeri",
+      date: "Aug 2020 - Mar 2023",
+      desc: "I Completed Graduation on BA Functional English",
+      degree: "Bachelor of Functional English",
+    },
+    {
+      id: 1,
+      img: "https://www.joonsquare.com/usermanage/image/business/nochat-higher-secondary-school-kozhikode-8361/nochat-higher-secondary-school-kozhikode-logo.png",
+      school: "Nochat Higher Secondary",
+      date: "Apr 2018 - Apr 2020",
+      desc: "I completed my class 12 high school education at Methodist School, Nochad, where I studied Biology Science.",
+      degree: "ISC(XII), Biology Science",
+    },
+    {
+      id: 2,
+      img: "https://www.yello.ae/img/ae/r/1694009453-35-international-indian-school.jpg",
+      school: "International Indian School, Ajman",
+      date: "-Apr 2018",
+      desc: "I Completed my school studies",
+      degree: "ICSC(X)",
+    },
+  ];
+  
+  export const projects = [
+    
+    {
+      id: 5,
+      title: "Todo App",
+      date: "Jun 2023",
+      description:
+        "A To-Do Application Developed using Flutter framework. User can add, Delete and mark as completed on each notes. If a todo is completed then a strike line will come on the completed ToDo. user can swipe the todo to delete a note.",
+      image:
+        "https://i.ytimg.com/vi/mMgr47QBZWA/maxresdefault.jpg",
+      tags: ["Flutter", "Local Storage", "Hive", "Dart"],
+      category: "web app",
+      github: "https://github.com/abidnk/ToDo_App/tree/main",
+      webapp: "https://drive.google.com/file/d/1jKYsJoK-lcwKIx7UBQbgrDNGx5wfzFS-/view?usp=drive_link",
+    },
+    {
+      id: 6,
+      title: "ToDo Web",
+      date: "Aug 2023",
+      description:
+        "A simple react app that can add delete and update todo notes",
+      image:
+        "https://miro.medium.com/v2/resize:fit:1400/1*-_RZQUIVlitKZ8-PaD_AcQ.jpeg",
+      tags: ["React Js", "Javascript", "Redux", "CSS3"],
+      category: "web app",
+      github: "https://github.com/abidnk/todo_redux",
+      webapp: "todoo-redux.netlify.app/",
+    },
+    {
+      id: 7,
+      title: "Baby Kart",
+      date: "Aug 2023 - sep 2023",
+      description:
+        "A React app designed for purchasing baby products, featuring functionalities such as an Add to Cart option, a payment section, and an admin dashboard. The admin section enables administrators to manage products by adding, deleting, and editing items. Additionally, the web application includes a user registration page and a login page.",
+      image:
+        "https://img.freepik.com/free-psd/baby-shop-landing-page-template_23-2148741790.jpg?size=626&ext=jpg&ga=GA1.1.1880011253.1699056000&semt=ais",
+      tags: ["React Js", "Redux", "CSS3", "HTML5", "Javascript" ],
+      category: "android app",
+      github: "https://github.com/abidnk/baby-kart",
+      webapp: "https://baby-kart.netlify.app",
+    },
+    {
+      id: 8,
+      title: "E-MOTORAD",
+      date: "Nov 2023 - Dec 2023",
+      description:
+  
+      "E-Motorad is a website created using React, Redux, and Axios for fetching data from the backend. Users can add items to their cart and wishlist, perform login and registration. The admin has functionalities such as adding, deleting, and editing products. Additionally, the admin can view user details, delete users, and access order information for users.",
+      image:
+        "https://m.media-amazon.com/images/S/stores-image-uploads-eu-prod/5/AmazonStores/A21TJRUUN4KGV/d9906666e6ab591c464163a5fd2c8fb4.w3000.h1500._CR0%2C0%2C3000%2C1500_SX3000_.jpg",
+      tags: ["React", "Redux", "Javascript", "HTML5", "CSS3", "Material UI"],
+      category: "machine learning",
+      github: "https://github.com/abidnk/E-commerce",
+      webapp: "https://e-motorad.vercel.app",
+    },
+    {
+      id: 8,
+      title: "To-Do",
+      date: "Dec 2023",
+      description:
+        "A simple TO-Do app made using Next.JS as fram work",
+      image:
+        "https://i.ytimg.com/vi/kjmY3Igzr-Q/maxresdefault.jpg",
+      tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+      category: "machine learning",
+      github: "https://github.com/abidnk/Todo_App_NextJS",
+      webapp: "https://github.com/abidnk/Todo_App_NextJS",
+    }
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
+  
